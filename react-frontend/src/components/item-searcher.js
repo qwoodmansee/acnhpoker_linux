e6a28899a633d9bf720a849e0e258133622fb3c0
Quinton Woodmansee <qwoodmansee@capital.edu>
2020-05-19 15:18:50 -0400

    debounce text field

--- react-frontend/src/components/item-searcher.js
+++ react-frontend/src/components/item-searcher.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ItemList from './item-list';
 import Container from '@material-ui/core/Container';
@@ -9,15 +9,17 @@ export default function ItemSearcher({options, onSelectedItemId, selectedItemId}
   const [filteredOptions, setFilteredOptions] = useState([]);
   const [userInput, setUserInput] = useState('');
 
-  const onSearchChange = useCallback((event) => {
-    const userInput = event.currentTarget.value;
-    const filteredOptions = options.filter(
-      (option) => option.name.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-    );
-    
-    setFilteredOptions(filteredOptions);
-    setUserInput(userInput);
-  }, [options]);
+  useEffect(() => {
+    if (!userInput) { return; }
+    const debounceTimeout = window.setTimeout(() => {
+      const filteredOptions = options.filter(
+        (option) => option.name.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+      );      
+      setFilteredOptions(filteredOptions);
+    }, 500);
+    return () => window.clearTimeout(debounceTimeout);
+  }, [userInput]);
+
 
   const onOptionClick = useCallback((e) => {
     const itemName = e.currentTarget.innerText.split(':')[0];
@@ -34,7 +36,7 @@ export default function ItemSearcher({options, onSelectedItemId, selectedItemId}
         id="search-box" 
         label="Item Name" 
         variant="outlined" 
-        onChange={onSearchChange}
+        onChange={(e) => setUserInput(e.target.value)}
         value={userInput}
       />
       <ItemList
